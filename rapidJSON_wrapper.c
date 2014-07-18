9a01e717ec10997bbe6c1e84a28d6dfdb760ed09
Kenneth Nierenhausen <knierenh@ucsd.edu>
2014-07-18 12:57:52 -0700

    Linked callbacks and allocated/freed structure

--- rapidJSON_wrapper.c
+++ rapidJSON_wrapper.c
@@ -27,7 +27,6 @@ typedef struct tm_json {
 } tm_json_t;
 
 /* prototypes */
-int initialize(tm_json_t*);
 int to_value(tm_json_t*, const char*);
 void cb_default(void);
 void cb_null(void);
@@ -43,18 +42,32 @@ void cb_endObject(void/*TODO*/);
 void cb_startArray(void);
 void cb_endArray(void/*TODO*/);
 
-/* links the function pointes */
-int initialize(tm_json_t* handler) {
-  printf("Initialized\n");
-}
-
 /* Parses the JSON string and calls those functions when it hits each token */
 int to_value(tm_json_t* handler, const char* json_stringified) {
 
-  /* link the function pointers to the callbacks */
-  initialize(handler);
+  /* allocate memory for the struct */
+  handler = (tm_json_t*)malloc(sizeof(tm_json_t));
 
-  /* 88 lines of parsing code that I got rid of... what goes here? */  
+  /* link the function pointers to the callbacks */
+  handler->fp_default = cb_default;
+  handler->fp_null = cb_null;
+  handler->fp_bool = cb_bool;
+  handler->fp_int = cb_int;
+  handler->fp_uint = cb_uint;
+  handler->fp_int64 = cb_int64;
+  handler->fp_uint64 = cb_uint64;
+  handler->fp_double = cb_double;
+  handler->fp_string = cb_string;
+  handler->fp_startObject = cb_startObject;
+  handler->fp_endObject = cb_endObject;
+  handler->fp_startArray = cb_startArray;
+  handler->fp_endArray = cb_endArray;
+
+  /* 88 lines of parsing code that I got rid of... what goes here? */
+
+  /* free the handler (this may need to be updated once implemented) */
+  free(handler);
+
   return 0;
 
 }
@@ -76,14 +89,14 @@ void cb_endArray(void/*TODO*/) { printf("END_ARRAY: todo\n"); }
 
 /* Temporaorily used to test to_value */
 int main(void) {
-/*  tm_json_t* handler;
-  printf("=======================================================================\n");
+  tm_json_t* handler;
+  printf("=====================================================================\n");
   to_value(handler,"{{}}");
-  printf("=======================================================================\n");
+/*  printf("=====================================================================\n");
   to_value(handler,"{\"a\":1,\"b\":{\"c\":2},\"d\":3}");
-  printf("=======================================================================\n");
+  printf("=====================================================================\n");
   to_value(handler,"{\"clientX\":39,\"clientY\":17,\"offsetX\":39,\"offsetY\":17}");
-  printf("=======================================================================\n");
+  printf("=====================================================================\n");
   to_value(handler,"{\"name\":\"Jim Cowart\",\"location\":{\"city\":{\"name\":\"Chattanooga\",\"population\":167674},\"state\":{\"name\":\"Tennessee\",\"abbreviation\":\"TN\",\"population\":6403000}},\"company\":\"appendTo\"}");
 */  return 0;
 }
